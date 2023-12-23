8fb64eb716ae9030d365d94214f388a53abbbd9f
terravidhal <infiniterraetincelle@gmail.com>
2023-12-23 04:13:04 +0100

    first_react_with_jsx_dojo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do :</p>
+      <ul>
+        <li>*Learn React</li>
+        <li>*Climb Mr Everest</li>
+        <li>*Run a Marathon</li>
+        <li>*Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
